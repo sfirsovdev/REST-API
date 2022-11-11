fca9ad06e27dbd979376e4aa17bba0289c5fa7f5
sfirsovdev <s.firsov1985@gmail.com>
2022-11-11 21:36:38 +0300

    Deploy

---
+++ schemas/contactSchema.js
@@ -0,0 +1,50 @@
+const { Schema, model, SchemaTypes } = require("mongoose");
+const Joi = require("joi");
+
+const handleSaveErrors = require("../helpers/handelSaveErrors");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleSaveErrors);
+
+const contactSchemaJoi = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(5).max(15).required(),
+  favorite: Joi.bool(),
+});
+
+const updateStatusJoiSchema = Joi.object({
+  favorite: Joi.bool().required(),
+});
+
+const Contact = model("Contact", contactSchema);
+
+module.exports = {
+  Contact,
+  contactSchemaJoi,
+  updateStatusJoiSchema,
+};
